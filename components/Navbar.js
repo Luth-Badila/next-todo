a15aa2d25567b6b991c8516defc51c1896f5381d
Luth-Badila <FrancescoLutfi10@gmail.com>
2023-04-28 22:13:39 +0700

    made todo web for next

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="uppercase bg-blue-400">
+      <p className="px-[8rem] py-4 text-white font-semibold">To do list app</p>
+    </div>
+  );
+}
+
+export default Navbar;
